37ea8404db93813c042ce036f198e5c82a04b45e
Joshua Byrd <phocks@gmail.com>
2021-03-13 12:34:24 +1000

    first commit

---
+++ App.svelte
@@ -0,0 +1,74 @@
+<!-- App.svelte -->
+<script>
+  import { onMount, onDestroy } from "svelte";
+
+  const MARKET = "XLM-PERP";
+
+  let socket = new WebSocket("wss://ftx.com/ws/");
+  let price = 0;
+
+  onMount(async () => {
+    socket.onopen = function (e) {
+      console.log("[open] Connection established");
+      console.log("Sending request to server");
+      socket.send(
+        JSON.stringify({
+          market: MARKET,
+          channel: "ticker",
+          op: "subscribe",
+        })
+      );
+    };
+
+    socket.onmessage = function (event) {
+      // console.log(`[message] Data received from server: ${event.data}`);
+      const eventData = JSON.parse(event.data);
+
+      if (eventData?.type === "update") {
+        price = eventData.data.last;
+      }
+    };
+
+    socket.onclose = function (event) {
+      if (event.wasClean) {
+        console.log(
+          `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
+        );
+      } else {
+        // e.g. server process killed or network down
+        // event.code is usually 1006 in this case
+        console.log("[close] Connection died");
+      }
+    };
+
+    socket.onerror = function (error) {
+      console.error(`[error] ${error.message}`);
+    };
+  });
+
+  onDestroy(async () => {
+    // socket.close(1000, "Work complete");
+  });
+</script>
+
+<div class="App">
+  <div class="Container">
+    <div>{MARKET}</div>
+    <div>${price}</div>
+  </div>
+</div>
+
+<style>
+  .App {
+    font-family: "Courier New", Courier, monospace;
+    font-size: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
+
+  .Container {
+    text-align: center;
+  }
+</style>
